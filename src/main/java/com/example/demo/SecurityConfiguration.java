f0fa4da90c4ae82e67aa0a7e161625d0a9b04fd3
satbirmehta <mehta.satbir@gmail.com>
2017-11-27 19:44:03 -0500

    Added Permissions

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends  WebSecurityConfigurerAdapter {
 
 
-
+/*
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -23,12 +23,43 @@ public class SecurityConfiguration extends  WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic();
 
-    }
+    }*/
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER")
+                .and()
+                .withUser("dave").password("davepassword").roles("ADMIN");
+    }
+
+
+
+    //Security 19 : with special permission
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      /*  http
+                .authorizeRequests()
+                .antMatchers("/")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .httpBasic();  */
+
+       http
+
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .httpBasic();
 
     }
 }
